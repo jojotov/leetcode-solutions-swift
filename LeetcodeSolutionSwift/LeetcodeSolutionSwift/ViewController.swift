7368da715349fce9d8a25538ef28aecab4592fe2
jojotov <jojodingtz@gmail.com>
2019-08-26 22:39:38 +0800

    add xcode project

---
+++ LeetcodeSolutionSwift/LeetcodeSolutionSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeetcodeSolutionSwift
+//
+//  Created by jojo on 2019/8/26.
+//  Copyright © 2019 jojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
